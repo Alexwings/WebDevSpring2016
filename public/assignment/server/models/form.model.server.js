79cd95edc31338e8a8d470d3dc3bda4ea68bdba8
Alexwings <wang.xinyuan@husky.neu.edu>
2016-04-01 13:13:38 -0400

    finish form model

--- public/assignment/server/models/form.model.server.js
+++ public/assignment/server/models/form.model.server.js
@@ -20,22 +20,54 @@ module.exports = function(mongoose){
     }
 
     function deleteForm(formId){
-
+        return FormModel.remove().where('_id').equals(formId);
     }
 
     function updateForm(formId, form){
-
+        var defer = q.defer();
+        FormModel.update(
+            {'_id': formId},
+            {$set: form},
+            function(err, stat){
+                if(!err){
+                    defer.resolve(stat);
+                }else{
+                    defer.reject(err);
+                }
+            }
+        );
+        return defer.promise;
     }
 
     function findFormById(formId){
-
+        return FormModel.findById(formId);
     }
 
     function findFormsForUser(userId){
-
+        var defer = q.defer();
+        FormModel.find(
+            {userId: userId},
+            function(err, doc){
+                if(!err){
+                    defer.resolve(doc);
+                }else {
+                    defer.reject(err);
+                }
+            }
+        );
+        return defer.promise;
     }
 
     function createForm(form){
-
+        var defer = q.defer();
+        FormModel.create(form,
+            function(err, doc){
+                if(!err){
+                    defer.resolve(doc);
+                }else{
+                    defer.reject(err);
+                }
+            });
+        return defer.promise;
     }
 }
